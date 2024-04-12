d3581b81353d25c5e880b5772fdd4a5bb9a2b7f6
KingTingTheGreat <jting@bu.edu>
2024-04-12 16:25:34 -0400

    commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, redirect, session, jsonify
+from flask import Flask, request, redirect, session, jsonify, send_file
 from dotenv import load_dotenv
 from flask_cors import CORS
 from PIL import Image
 import io
 import os
 from ImageToColors import ImageToColors
+import base64
 
 load_dotenv()
 
@@ -19,29 +20,37 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})  # to be changed to the ac
 # def hello_world():
     # return "<p>Hello, World!</p>"
 
+def downscale(image):
+    r = 100 / image.width
+    return image.resize((int(image.width * r), int(image.height * r)))
+
 @app.route("/trans", methods=['POST'])
 def trans():
-    if 'image' not in request.files:
+    image = request.files.get('input-image', None)
+    if image is None:
+        print('no image provided')
         return jsonify({'error': 'no image provided'}), 400
-    
-    image = request.files['image']
-
     extension = image.filename.split('.')[-1]
 
     if extension not in ALLOWED_EXTENSIONS:
+        print('invalid file type')
         return jsonify({'error': 'invalid file type'}), 400
     
     image = Image.open(image)
 
     # process image here
     # (pooja this is where you come in)
-    processed_image = image
+    processed_image = downscale(image)
 
     response = processed_image.convert('RGB')
     response_io = io.BytesIO()
     response.save(response_io, format='PNG')
     response_io.seek(0)
-    return jsonify({'processed_image': response_io.read().hex()}), 200
+    processed_image_data = base64.b64encode(response_io.getvalue()).decode('utf-8')
+
+    print('returning processed image')
+    return send_file(response_io, mimetype='image/png')
+    # return jsonify({'processedImage': processed_image_data}), 200
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,50 @@
-document.getElementById("image-form").addEventListener("submit", function (e) {
-    e.preventDefault();
+const imageForm = document.getElementById("image-form");
+imageForm.addEventListener("submit", processImage);
+
+function processImage(event) {
+    event.preventDefault();
 
     const formData = new FormData(this);
+    const URL = "http://127.0.0.1:8888/trans"
 
-    fetch("http://127.0.0.1:5000/trans", {
+    console.log(formData.get("input-image"));
+
+    fetch(URL, {
         method: 'POST',
         body: formData
     })
-        .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                console.log(data.error);
+        .then(response => {
+            if (response.ok) {
+                let processedImage = document.getElementById("processed-image");
+                processedImage.src = response.blob();
+                processedImage.style.visibility = "visible";
+                // console.log(response.blob());
             }
             else {
-                const processedImage = document.getElementById("processed-image");
-                processedImage.src = 'data:image/png;base64,' + data.processedImage;
-                processedImage.style.display = "block";
+                console.log(`an error occurred`);
+                console.log(`Error ${response.status}: ${response.statusText}`);
             }
-        });
-});
+        })
+        .catch(error => {
+            console.log(`an error occurred`);
+            console.log(error);
+        }
+        );
+}
+//         .then(response => response.json())
+//         .then(data => {
+//             if (data.error) {
+//                 console.log(`an error occurred`)
+//                 console.log(data.error);
+//             }
+//             else {
+//                 console.log(`success`);
+//                 console.log(data);
+//                 console.log(data.processedImage !== null);
+//                 let processedImage = document.getElementById("processed-image");
+//                 processedImage.src = 'data:image/png;base64,' + data.processedImage;
+//                 // processedImage.src = data.processedImage;
+//                 processedImage.style.visibility = "visible";
+//             }
+//         });
+// }
